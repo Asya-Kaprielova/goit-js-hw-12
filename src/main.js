6bea225cd628416b616e198637b873f30e7cac86
Asya <nastena.kaprielova209@gmail.com>
2026-06-28 20:12:25 +0300

    first try

---
+++ src/main.js
@@ -0,0 +1,53 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+import { getImagesByQuery } from './js/pixabay-api.js';
+import { createGallery, clearGallery, showLoader, hideLoader } from './js/render-functions.js';
+
+const searchForm = document.querySelector('.form');
+
+searchForm.addEventListener('submit', handleSearch);
+
+function handleSearch(event) {
+  event.preventDefault();
+
+  const form = event.currentTarget;
+  const searchQuery = form.elements['search-text'].value.trim();
+
+  if (searchQuery === '') {
+    iziToast.warning({
+      title: 'Caution',
+      message: 'Please enter a valid search term!',
+      position: 'topRight',
+    });
+    return;
+  }
+
+  clearGallery();
+  showLoader();
+
+  getImagesByQuery(searchQuery)
+    .then(data => {
+      if (data.hits.length === 0) {
+        iziToast.error({
+          message: 'Sorry, there are no images matching your search query. Please try again!',
+          position: 'topRight',
+          timeout: 4000,
+        });
+        return;
+      }
+
+      createGallery(data.hits);
+    })
+    .catch(error => {
+      iziToast.error({
+        title: 'Error',
+        message: `Something went wrong: ${error.message}`,
+        position: 'topRight',
+      });
+    })
+    .finally(() => {
+      hideLoader();
+      form.reset(); 
+    });
+}
